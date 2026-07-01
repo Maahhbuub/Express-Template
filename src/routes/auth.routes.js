604b97b1eaeff5e4e019426cc5043516c8410847
Mahbub <mrahman221441@gmail.com>
2026-07-01 16:24:47 +0600

    refactor(auth): add-router-route

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -12,15 +12,20 @@ import { protect } from '../middlewares/auth.middleware.js';
 // controller
 import { register, login, refreshAccessToken, getMe, logout, } from '../controllers/auth.controller.js';
 
-// router.route('/')
-//     .post(catchAsync(register));
+router.route('/me')
+    .get(protect, catchAsync(getMe));
 
-router.get('/me', protect, catchAsync(getMe));
+router.route('/register')
+    .post(validateRequest(createUserSchema), catchAsync(register));
 
-router.post('/register', validateRequest(createUserSchema), catchAsync(register));
-router.post('/login', validateRequest(loginUserSchema), catchAsync(login));
-router.post('/refresh-token', catchAsync(refreshAccessToken));
-router.post('/logout', protect, catchAsync(logout));
+router.route('/login')
+    .post(validateRequest(loginUserSchema), catchAsync(login));
+
+router.route('/refresh-token')
+    .post(catchAsync(refreshAccessToken));
+
+router.route('/logout')
+    .post(protect, catchAsync(logout));
 
 
 export default router;
